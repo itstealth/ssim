95f1cf6326fda5d5249b1856be7e4340e34be8df
DanishCodebase <ammarsaifi5376@gmail.com>
2026-03-20 18:11:52 +0530

    refactor: replace Next.js Image component with a native `img` tag for the call-now icon.

--- src/components/DockButtons.jsx
+++ src/components/DockButtons.jsx
@@ -44,7 +44,7 @@ export function DockButtons() {
         </DockIcon> */}
         <DockIcon size="48" className="bg-black/10 dark:bg-white/10">
           <Link href="tel:+919391114948" target="_blank" rel="noopener noreferrer">
-            <Image src="/call-now.png" alt="phone" width={36} height={36} />
+            <img src="/call-now.png" alt="phone" width={36} height={36} />
           </Link>
         </DockIcon>
 
